06f2c1f50bebbf650f58eae3e530a19bd5834040
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-24 18:06:29 -0300

    Creating the state tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,11 +8,13 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 // Factory function to create a ShallowWrapper for the App Component-app
 // @function setup
 // @param {object} props - Component props specific to this setup
-// @param {any} state - initial state for setup
+// @param {object} state - initial state for setup
 // @returns {ShallowWrapper}
 
 const setup = ( props={}, state=null) => {
-  return shallow(<App {...props} />);
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper
 }
 
 // Return ShallowWrapper containing node(s) with the given data-test value.
@@ -43,10 +45,22 @@ test('renders counter display', () => {
 });
 
 test('counter starts at 0', () => {
-
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
 });
 
 test('clicking button increments counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
 
+  //find button and click
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  wrapper.update();
+
+  //find display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1)
 });
 
