9fe9f60c91817a87ad8e5387360dc8e7f0b329ed
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-24 13:03:59 -0300

    Applying DRY principles in tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,20 +5,47 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+// Factory function to create a ShallowWrapper for the App Component-app
+// @function setup
+// @param {object} props - Component props specific to this setup
+// @param {any} state - initial state for setup
+// @returns {ShallowWrapper}
+
+const setup = ( props={}, state=null) => {
+  return shallow(<App {...props} />);
+}
+
+// Return ShallowWrapper containing node(s) with the given data-test value.
+// @param {ShadowWrapper} wrapper - Enzyme shallow wrapper to search within.
+// @param {string} val - Value of data-test attribute for search.
+// @return {ShallowWrapper}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+} 
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr( wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
+
 test('renders increment button', () => {
-
+  const wrapper = setup();
+  const appComponent = findByTestAttr( wrapper, 'increment-button');
+  expect(appComponent.length).toBe(1);
 });
+
 test('renders counter display', () => {
-
+  const wrapper = setup();
+  const appComponent = findByTestAttr( wrapper, 'counter-display');
+  expect(appComponent.length).toBe(1);
 });
+
 test('counter starts at 0', () => {
 
 });
+
 test('clicking button increments counter display', () => {
 
 });
